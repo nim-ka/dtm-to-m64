9f7cf566eaea35898b2f0e005b1b0ae7bdcf752e
nim from KA <35317534+nim-ka@users.noreply.github.com>
2018-09-01 17:33:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main () {
 	FILE *dtm;
 	FILE *m64;
-	struct gcController dtmBuffer;
-	struct n64Controller m64Buffer;
+	struct gcController dtmBuffer[1];
+	struct n64Controller m64Buffer[1];
 	
 	dtm = fopen("input.dtm", "r");
 	m64 = fopen("output.m64", "w");
@@ -16,7 +16,7 @@ int main () {
 	fseek(m64, 0x300, SEEK_SET);
 	
 	while (fread(dtmBuffer, 8, 1, dtm)) {
-		printf(dtmBuffer.buttonA ? "A" : " ");
+		printf(dtmBuffer[0].buttonA ? "A" : " ");
 	}
 	
 	return 0;
