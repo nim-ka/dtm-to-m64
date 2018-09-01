883346945913e35ce78230cd56bf155907b4e4b2
nim from KA <35317534+nim-ka@users.noreply.github.com>
2018-09-01 17:03:48 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
+#include "controller.h"
 #include <stdio.h>
 
 int main () {
 	FILE *dtm;
-	char buffer[8];
+	FILE *m64;
+	gcController dtmBuffer;
 	
 	dtm = fopen("input.dtm", "r");
+	m64 = fopen("output.m64", "w");
 	
 	if (dtm == NULL) return 1;
 	
-	fseek(dtm, 0x300, SEEK_SET);
+	fseek(dtm, 0x100, SEEK_SET);
+	fseek(m64, 0x300, SEEK_SET);
 	
-	while (fread(buffer, 1, 8, dtm)) {
-		printf(buffer);
+	while (fread(dtmBuffer, 8, 1, dtm)) {
+		printf(dtmBuffer.buttonA ? "A" : " ");
 	}
 	
 	return 0;
