445d64c0bb863b776847809967525fe1be2c7ae3
nim from KA <35317534+nim-ka@users.noreply.github.com>
2018-09-01 17:54:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "controllers.h"
 #include <stdio.h>
+#include <math.h>
 
 int main () {
 	FILE *dtm;
 	FILE *m64;
+	
 	struct gcController dtmBuffer[1];
 	struct n64Controller m64Buffer[1];
+	
+	double cStickAngle;
 	
 	dtm = fopen("input.dtm", "r");
 	m64 = fopen("output.m64", "w");
@@ -16,7 +20,27 @@ int main () {
 	fseek(m64, 0x300, SEEK_SET);
 	
 	while (fread(dtmBuffer, 8, 1, dtm)) {
-		printf(dtmBuffer[0].buttonA ? "A" : " ");
+		cStickAngle = atan2(dtmBuffer[0].cStickY - 128, dtmBuffer[0].cStickX - 128) * (180.0 / M_PI);
+		
+		m64Buffer[0] = {
+			.buttonA: dtmBuffer[0].buttonA,
+			.buttonB: dtmBuffer[0].buttonB,
+			.buttonZ: dtmBuffer[0].buttonZ,
+			.buttonS: dtmBuffer[0].buttonS,
+			.dpadUp: dtmBuffer[0].dpadUp,
+			.dpadDown: dtmBuffer[0].dpadDown,
+			.dpadLeft: dtmBuffer[0].dpadLeft,
+			.dpadRight: dtmBuffer[0].dpadRight,
+			.reserved: 0x0,
+			.buttonL: dtmBuffer[0].buttonL,
+			.buttonR: dtmBuffer[0].buttonR,
+			.cUp: cStickAngle >= 45 && cStickAngle <= 135,
+			.cDown: cStickAngle >= 225 && cStickAngle <= 315,
+			.cLeft: cStickAngle >= 135 && cStickAngle <= 225,
+			.cRight: cStickAngle >= 315 || cStickAngle <= 45
+		};
+		
+		fwrite(m64Buffer, 4, 1, m64);
 	}
 	
 	return 0;
