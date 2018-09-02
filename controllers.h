e31eabd3416a045e6ba0715f69b60b4703041d25
nim from KA <35317534+nim-ka@users.noreply.github.com>
2018-09-01 21:50:02 -0400

    Update controllers.h

--- controllers.h
+++ controllers.h
@@ -1,34 +1,28 @@
 struct gcController {
-	int buttonS: 1;
-	int buttonA: 1;
-	int buttonB: 1;
-	int buttonX: 1;
-	int buttonY: 1;
-	int buttonZ: 1;
-	int dpadUp: 1;
-	int dpadDown: 1;
-	int dpadLeft: 1;
-	int dpadRight: 1;
-	int buttonL: 1;
-	int buttonR: 1;
-	int reserved: 4;
-	unsigned int pressureL: 8;
-	unsigned int pressureR: 8;
-	unsigned int joystickX: 8;
-	unsigned int joystickY: 8;
-	unsigned int cStickX: 8;
 	unsigned int cStickY: 8;
+	unsigned int cStickX: 8;
+	unsigned int joystickY: 8;
+	unsigned int joystickX: 8;
+	unsigned int pressureR: 8;
+	unsigned int pressureL: 8;
+	int reserved: 4;
+	int buttonR: 1;
+	int buttonL: 1;
+	int dpadRight: 1;
+	int dpadLeft: 1;
+	int dpadDown: 1;
+	int dpadUp: 1;
+	int buttonZ: 1;
+	int buttonY: 1;
+	int buttonX: 1;
+	int buttonB: 1;
+	int buttonA: 1;
+	int buttonS: 1;
 };
 
 struct n64Controller {
-	int buttonA: 1;
-	int buttonB: 1;
-	int buttonZ: 1;
-	int buttonS: 1;
-	int dpadUp: 1;
-	int dpadDown: 1;
-	int dpadLeft: 1;
-	int dpadRight: 1;
+	int joystickY: 8; // signed
+	int joystickX: 8; // signed
 	int reserved: 2;
 	int buttonL: 1;
 	int buttonR: 1;
@@ -36,6 +30,12 @@ struct n64Controller {
 	int cDown: 1;
 	int cLeft: 1;
 	int cRight: 1;
-	unsigned int joystickX: 8;
-	unsigned int joystickY: 8;
+	int buttonA: 1;
+	int buttonB: 1;
+	int buttonZ: 1;
+	int buttonS: 1;
+	int dpadUp: 1;
+	int dpadDown: 1;
+	int dpadLeft: 1;
+	int dpadRight: 1;
 };
